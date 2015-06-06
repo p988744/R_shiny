016680f3670326dd8468f86af5b160905d902974
weifan <weifan@dataworkplace.net>
2015-06-06 03:27:24 -0400

    從ui,R讀取資料

--- server.R
+++ server.R
@@ -1,2 +1,10 @@
 shinyServer(function(input, output) {
+  
+    output$text1 <- renderText({
+      paste(
+        "You have selected this", input$var, ",",
+        "From ", input$range[1], " to ", input$range[2]
+        )
+    })
+  
 })
